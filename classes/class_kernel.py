I fixed the kernel. The scaling was wrong.

classes/class_kernel.py
@@ -63,17 +63,19 @@ class Kernel:
     def __repr__(self):
         return repr(self.fct_kernel)
 
-    def eval(self, T_t, eval_point):
+    def eval(self, T_t, eval_point, T_max):
         # optimize cool to optimize using numpy and vectorize.
         length_elements_T_t = [len(T_t[i]) for i in range(len(T_t))]
+        # ans is the kernel evaluated on the jumps
         ans = self.fct_kernel(T_t=T_t, eval_point=eval_point, length_elements_T_t=length_elements_T_t,
                               **{k: self.__dict__[k] for k in self.__dict__ if
                                  k in signature(self.fct_kernel).parameters})
+        # ans is a list of np arrays.
+        # then I want to scale every vector.
+        # The total integral should be T_max, so I multiply by T_max
+
         for i in range(len(length_elements_T_t)):
-            value = np.sum(ans[i])
-            scale_factor = length_elements_T_t[i]
-            factor = scale_factor / value
-            ans[i] = ans[i] * factor  # *= do not work correctly since the vectors are not the same type.
+            ans[i] = ans[i] * T_max  # *= do not work correctly since the vectors are not the same type (int/float).
         return ans
 
 
@@ -87,9 +89,9 @@ def fct_top_hat(T_t, length_elements_T_t, eval_point, a=-200, b=200):
     return output
 
 
-def fct_plain(T_t, length_elements_T_t, eval_point):
+def fct_plain(T_t, length_elements_T_t, eval_point, T_max):
     return [
-        np.full(length_elements_T_t[i], 1)
+        np.full(length_elements_T_t[i], 1/T_max)
         for i in range(len(length_elements_T_t))
     ]
 

functions_MLE.py
@@ -11,10 +11,12 @@ from classes.class_hawkes_process import *
 
 
 # the function returns a flag for the reason beeing that if it failed to converge too many times, it s perhaps better to try on a new data set.
+# now there is an error raised.
 def call_newton_raph_MLE_opt(T_t, T, w=None, silent=True):
+    # w shouldn't be None, however as a safety measure, just before doing the computations !
     if w is None:
-        w = Kernel(fct_plain, "plain").eval(T_t,
-                                            0)  # eval point equals 0 because, if the weights haven't been defined earlier, it means we don't care when we estimate.
+        w = Kernel(fct_plain, "plain", T_max =  T).eval(T_t, 0, T_max = T)
+        # eval point equals 0 because, if the weights haven't been defined earlier, it means we don't care when we estimate.
 
     M = len(T_t)
     MU = np.full(M, 0.1)
@@ -95,12 +97,13 @@ def estimation_hp(hp, estimator, T_max, nb_of_guesses, kernel_weight= kernel_pla
 def simulation_and_convergence(T_max, hp, kernel_weight, silent, time_estimation):
 
     intensity, time_real = hp.simulation_Hawkes_exact(T_max=T_max, plot_bool=False, silent=True)
-    w = kernel_weight.eval(T_t=time_real, eval_point=time_estimation)
+    w = kernel_weight.eval(T_t=time_real, eval_point=time_estimation, T_max = T_max)
     try:
         alpha_hat, beta_hat, mu_hat = call_newton_raph_MLE_opt(time_real, T_max, w, silent=silent)
     except Error_convergence as err:
         warnings.warn(err.message)
         return simulation_and_convergence(T_max, hp, kernel_weight, silent, time_estimation)
+    # One shouldn't get an infinite loop here. It's probability.
     return alpha_hat, beta_hat, mu_hat
 
 