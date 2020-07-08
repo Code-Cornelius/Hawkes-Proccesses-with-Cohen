d726caf8278238a3ee38bf6f85acf37423c9d72e
monthelicon <52886695+monthelicon@users.noreply.github.com>
2020-07-08 16:04:44 +0100

    New plots.

--- test/test_my_images.py
+++ test/test_my_images.py
@@ -35,7 +35,7 @@ class Test_images(unittest.TestCase):
     def tearDown(self):
         plt.show()
 
-    def test_image_different_kernel_vision(self):
+    def test_image_FKDE_AKDE_CKDE(self):
         nb_of_points = 10000
         ############################## 1
         xx = np.linspace(-15, 15, nb_of_points)
@@ -51,7 +51,7 @@ class Test_images(unittest.TestCase):
         print(np.sum(zz * mesh))
 
         my_plot.uni_plot(0, xx, zz, dict_plot_param={'color': 'r', 'label': 'KDE'})
-        my_plot.set_dict_fig(0, {'xlabel': '', 'ylabel': 'Probability', 'title': 'KDE estimation, fixed size kernel'})
+        my_plot.set_dict_fig(0, {'xlabel': 'Time $t$', 'ylabel': 'Probability', 'title': 'KDE estimation, fixed size kernel'})
         my_plot.show_legend()
 
         ############################## 2
@@ -65,7 +65,7 @@ class Test_images(unittest.TestCase):
 
         my_plot.uni_plot(0, xx, zz, dict_plot_param={'color': 'r', 'label': 'KDE'})
         my_plot.set_dict_fig(0,
-                             {'xlabel': '', 'ylabel': 'Probability', 'title': 'KDE estimation, adaptive size kernel'})
+                             {'xlabel': 'Time $t$', 'ylabel': 'Probability', 'title': 'KDE estimation, adaptive size kernel'})
         my_plot.show_legend()
 
         print(np.sum(zz * mesh))
@@ -79,7 +79,7 @@ class Test_images(unittest.TestCase):
                          np.linspace(-0.004, max_x, len(xx)),
                          dict_plot_param={'color': 'g', 'label': 'Estimation point',
                                           'linestyle': '--', 'linewidth': 2,
-                                          'markersize': 0, 'label': 'Point of interest'})
+                                          'markersize': 0})
         points = np.array([-1.1, 0.5, 5.])
         for f in points:
             my_plot.uni_plot(0, [f for _ in xx],
@@ -127,11 +127,48 @@ class Test_images(unittest.TestCase):
             previous_f += recurrent_functions.phi_numpy(f, 0, 2) / 3
 
         my_plot.set_dict_fig(0,
-                             {'xlabel': '', 'ylabel': 'Probability',
-                              'title': 'Kernels represented as functions of the events'})
+                             {'xlabel': 'Time $t$', 'ylabel': 'Probability',
+                              'title': 'Kernel represented as function of the time'})
         my_plot.set_dict_fig(1,
-                             {'xlabel': '', 'ylabel': '', 'title': 'Kernels represented as functions of the time'})
+                             {'xlabel': 'Time event $t_i$', 'ylabel': '', 'title': 'Kernel represented as function of events $t_i$'})
         my_plot.show_legend()
+
+    def test_image_CKDE(self):
+        nb_of_points = 10000
+        xx = np.linspace(-15, 15, nb_of_points)
+        mesh = 30. / nb_of_points
+
+        ############################## 3
+        ############### left
+        zz = np.zeros(nb_of_points)
+        max_x = 0.23
+        my_plot = plot_functions.APlot(how=(1, 2), sharey=True)
+        my_plot.uni_plot(0, [0 for _ in xx],
+                         np.linspace(-0.004, max_x, len(xx)),
+                         dict_plot_param={'color': 'g', 'label': 'Estimation point',
+                                          'linestyle': '--', 'linewidth': 2,
+                                          'markersize': 0})
+        yy = (recurrent_functions.phi_numpy(xx, -4, 3) + recurrent_functions.phi_numpy(xx, 1, 1))/2
+        my_plot.uni_plot(0, xx, yy,  dict_plot_param={'color': 'r', 'label': 'Kernel for $t_i = 0$'})
+
+        ############### right
+        zz = (recurrent_functions.phi_numpy(-xx, -4, 3) + recurrent_functions.phi_numpy(-xx, 1, 1))/2
+        my_plot.uni_plot(1, [0 for _ in xx],
+                         np.linspace(-0.004, max_x, len(xx)),
+                         dict_plot_param={'color': 'g', 'label': 'Estimation point',
+                                          'linestyle': '--', 'linewidth': 2,
+                                          'markersize': 0})
+        my_plot.uni_plot(1, xx, zz, dict_plot_param={'color': 'r', 'label': 'Kernel for $t = 0$'})
+
+
+        my_plot.set_dict_fig(0,
+                             {'xlabel': 'Time t', 'ylabel': 'Probability',
+                              'title': 'Kernel represented as function of the time'})
+        my_plot.set_dict_fig(1,
+                             {'xlabel': 'Time event $t_i$', 'ylabel': '', 'title': 'Kernel represented as function of events $t_i$'})
+        my_plot.show_legend()
+
+
 
     def test_my_geometric_kernel(self):
         # des listes de 50 éléments
