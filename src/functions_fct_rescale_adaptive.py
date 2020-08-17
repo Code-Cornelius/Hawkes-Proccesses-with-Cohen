db0d4f30c5590134aede8927970641160bd9962f
monthelicon <52886695+monthelicon@users.noreply.github.com>
2020-08-17 02:28:12 +0100

    the tolerance parameter for dimensions where change or not.

--- src/functions_fct_rescale_adaptive.py
+++ src/functions_fct_rescale_adaptive.py
@@ -15,6 +15,9 @@ np.random.seed(124)
 
 
 def my_rescale_sin(value_at_each_time, G, L=None, R=None, h=3, l=0.2, silent = True):
+    if not G: # G == 0, it happens if no norm computed.
+        return np.full(len(value_at_each_time), 1)
+
     if L is None:
         L = np.quantile(value_at_each_time, 0.15)
     if R is None:
@@ -60,8 +63,21 @@ def rescale_min_max(vect):
     ans = [ (vect[i] - the_mean) /(the_max - the_min) + 1  for i in range(len(vect))]
     return ans
 
+def check_evoluating(vector, tol):
+    ''' if all values of the vector are inside the tube mean +/- tol, return false.
+    Args:
+        vector:
+        tol:
 
-def rescaling_kernel_processing(times, first_estimate,considered_param, silent = True):
+    Returns:
+
+    '''
+    the_mean = classical_functions.mean_list(vector)
+    if all(element < the_mean + tol for element in vector) and all(element > the_mean - tol for element in vector):
+        return False
+    return True
+
+def rescaling_kernel_processing(times, first_estimate,considered_param, tol = 0, silent = True):
     #todo change considered param for the tol and which_moving_parameters
 
     # on the first entry, I get the time, on the second entry I get nu alpha or beta, then it s where in the matrix.
@@ -75,48 +91,46 @@ def rescaling_kernel_processing(times, first_estimate,considered_param, silent =
 
     # the dimension of the data.
     M = len(first_estimate[0][0])
-    if considered_param == ['nu']:
-        total_M = M
-        # I am creating a vector with 2M*M + M entries, each one is going to be scaled, and this is the parameters I am using afterwards.
-        vect_of_estimators = [[] for _ in range(total_M)]
-        for k in range(len(times)):
-            for i in range(M):
-                vect_of_estimators[i].append(first_estimate[k][0][i])
-    elif considered_param == ['nu', 'alpha']:
-        total_M = M*M + M
-        # I am creating a vector with 2M*M + M entries, each one is going to be scaled, and this is the parameters I am using afterwards.
-        vect_of_estimators = [[] for _ in range(total_M)]
-        for k in range(len(times)):
-            for i in range(M):
-                vect_of_estimators[i].append(first_estimate[k][0][i])
-                for j in range(M):
-                    vect_of_estimators[M + i + j].append(first_estimate[k][1][i][j])
-    else :  #if considered_param == ['nu', 'alpha', 'beta']:
-        total_M = 2*M*M + M
-        # I am creating a vector with 2M*M + M entries, each one is going to be scaled, and this is the parameters I am using afterwards.
-        vect_of_estimators = [[] for _ in range(total_M)]
-        for k in range(len(times)):
-            for i in range(M):
-                vect_of_estimators[i].append(first_estimate[k][0][i])
-                for j in range(M):
-                    vect_of_estimators[M + i + j].append(first_estimate[k][1][i][j])
-                    vect_of_estimators[M + M * M + i + j].append(first_estimate[k][2][i][j])
+    total_M = 2*M*M + M
+    include_estimation = [False]* total_M
+    # I am creating a vector with 2M*M + M entries, each one is going to be scaled, and this is the parameters I am using afterwards.
+    vect_of_estimators = [[] for _ in range(total_M)]
+    for k in range(len(times)):
+        for i in range(M):
+            vect_of_estimators[i].append(first_estimate[k][0][i])
+            for j in range(M):
+                vect_of_estimators[M + i + j].append(first_estimate[k][1][i][j])
+                vect_of_estimators[M + M * M + i + j].append(first_estimate[k][2][i][j])
 
-
-    # perhaps not include the betas
-    rescale_vector = [0]* (total_M)
     for i in range(total_M):
-        rescale_vector[i] = rescale_min_max(vect_of_estimators[i])
+        # check the parameters I need to check.
+        if i < M and 'nu' in considered_param:
+            include_estimation[i] = True
+        elif i < M + M*M and 'alpha' in considered_param:
+            include_estimation[i] = True
+        elif 'beta' in considered_param:
+            include_estimation[i] = True
+
+        if include_estimation[i]:
+            if not check_evoluating(vect_of_estimators[i],tol): # we don't keep the True
+                include_estimation[i] = False
+    if not silent:
+        print("which dim to include for norm : ", include_estimation)
+
+    rescale_vector = []
+    for i in range(total_M):
+        if include_estimation[i]:
+            rescale_vector.append(rescale_min_max(vect_of_estimators[i]))
 
     for j in range(len(times)):
-        ans[j] = np.linalg.norm([rescale_vector[i][j] for i in range(total_M)] , 2)
+        ans[j] = np.linalg.norm([rescale_vector[i][j] for i in range(len(rescale_vector) )], 2)
     # I compute the geometric mean from our estimator.
     G = gmean(ans)
     if not silent:
         print("vect  :", vect_of_estimators)
-        print("interm :", rescale_vector)
+        #print("interm :", rescale_vector)
         print("the norms ", ans)
-        print('mean : ', G)
+        #print('mean : ', G)
     scaling_factors = my_rescale_sin(ans, G=G, silent = silent)
     return scaling_factors
 
@@ -130,10 +144,13 @@ def creator_list_kernels(my_scalings, previous_width):
     return list_of_kernels
 
 
-def creator_kernels_adaptive(my_estimator_mean_dict, Times, considered_param, half_width, silent = True):
+def creator_kernels_adaptive(my_estimator_mean_dict, Times, considered_param, half_width, tol = 0.1, silent = True):
     # by looking at the previous estimation, we deduce the scaling
     # for that I take back the estimate
     # there is a problem of data compatibility, so I put the keys as integers, assuming that there is no estimation on the same integer.
+
+    #tolerance is by how much the dimension has to move in order to consider that it is worth updating wrt to it.
+
     my_estimator_dict = my_estimator_mean_dict.mean(separator='time estimation')  # take back the value of the estimation at a given time.
     my_estimator_dict = {int(key): my_estimator_dict[key] for key in my_estimator_dict.keys()}
     list_of_estimation = []
@@ -142,7 +159,9 @@ def creator_kernels_adaptive(my_estimator_mean_dict, Times, considered_param, ha
         a_time = int(a_time)
         list_of_estimation.append(my_estimator_dict[a_time])
 
-    my_scaling = rescaling_kernel_processing(Times, list_of_estimation, considered_param, silent = silent)
+    my_scaling = rescaling_kernel_processing(
+        times = Times, first_estimate = list_of_estimation,
+        considered_param = considered_param, tol = tol, silent = silent)
     if not silent:
         print('the scaling : ', my_scaling)
     # the kernel is taken as biweight.
